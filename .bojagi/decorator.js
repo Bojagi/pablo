19c07242ec0265cf4cf05136839df64e38c43e22
Simon Jentsch <simon@bojagi.io>
2021-02-12 02:00:27 +0100

    Remove Bojagi specific configs

--- .bojagi/decorator.js
+++
@@ -1,3 +0,0 @@
-import { PabloThemeProvider } from '../src/theme';
-
-export default (Story) => <PabloThemeProvider><Story/></PabloThemeProvider>;
--- .bojagirc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  webpackConfig: './.bojagi/webpack.config.js',
-}
