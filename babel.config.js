3100ed634f85b4d964459806012c8a33b6eae160
Simon Jentsch <simon@bojagi.io>
2021-02-13 16:46:14 +0100

    chore: Add build process (#9)
    * chore: Add build process
    
    * Revert CI related change (not needed)

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,34 @@
-module.exports = {
-  presets: [
-    '@babel/typescript',
-    '@babel/react',
-    [
-      '@babel/env',
-      {
-        modules: false,
-        targets: {
-          browsers: ['last 2 versions'],
+module.exports = (api) => {
+  const useESModules = api.env(['esm', 'rollup']);
+  const modernJS = api.env(['esm', 'es']);
+
+  const basePresets = modernJS
+    ? []
+    : [
+        [
+          '@babel/env',
+          {
+            modules: useESModules ? false : 'commonjs',
+            targets: {
+              browsers: ['last 2 versions'],
+            },
+          },
+        ],
+      ];
+
+  return {
+    presets: [...basePresets, '@babel/typescript', '@babel/react'],
+    plugins: [
+      [
+        '@babel/plugin-transform-typescript',
+        {
+          isTSX: true,
         },
-      },
+      ],
+      'babel-plugin-styled-components',
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-optional-chaining',
     ],
-  ],
-  plugins: [
-    [
-      '@babel/plugin-transform-typescript',
-      {
-        isTSX: true,
-      },
-    ],
-    'babel-plugin-styled-components',
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-optional-chaining'
-  ],
+  };
 };
