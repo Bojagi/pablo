62bd51610374cd6c4fac3bfa63e447924422f68d
Simon Jentsch <simon@bojagi.io>
2021-02-15 22:27:15 +0100

    Add error when there are more than just 0 matching workflow

--- scripts/getDraftReleaseVersion.js
+++ scripts/getDraftReleaseVersion.js
@@ -7,18 +7,25 @@ const octokit = new Octokit({
 
 async function run() {
   const { data } = await octokit.repos.listReleases({ owner: 'bojagi', repo: 'pablo' });
-  const draftRelease = process.env.VERSION_SHOULD_MATCH
+  const draftReleases = process.env.VERSION_SHOULD_MATCH
     ? // Should match the passed version
-      data.find((r) => r.draft && r.tag_name === `v${process.env.VERSION_SHOULD_MATCH}`)
+      data.filter((r) => r.draft && r.tag_name === `v${process.env.VERSION_SHOULD_MATCH}`)
     : // Needs to have a tag name attached
-      data.find((r) => r.draft && !!r.tag_name);
+      data.filter((r) => r.draft && !!r.tag_name);
 
-  if (!draftRelease) {
+  if (!draftReleases.length === 0) {
     console.error('No matching draft found');
     return process.exit(1);
   }
 
-  const version = draftRelease.tag_name.replace(/^v/, '');
+  if (draftReleases.length > 1) {
+    console.error(
+      'There are multiple draft releases, please run this workflow again with a specific version number'
+    );
+    return process.exit(1);
+  }
+
+  const version = draftReleases[0].tag_name.replace(/^v/, '');
   process.stdout.write(version);
 }
 
