765ff1c8556c0cbcfdd02a8064a29602bf49337f
Simon Jentsch <simon@bojagi.io>
2021-02-18 12:56:42 +0100

    Fix animation story

--- src/animation/animation.stories.tsx
+++ src/animation/animation.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { css } from 'styled-components';
 import { Box } from '../Box';
 import { Card } from '../Card';
 import { Checkbox } from '../Checkbox';
@@ -15,23 +16,50 @@ const BaseStory = (args) => {
   const [visible, setVisible] = React.useState(false);
 
   return (
-    <Box>
-      <Box mb={4}>
-        <Checkbox mb={1} checked={visible} onChange={() => setVisible((v) => !v)} label="visible" />
+    <Box
+      flex
+      css={css`
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+      `}
+      alignItems="stretch"
+    >
+      <Box flex justifyContent="center" alignItems="center" flexGrow={1} flexBasis={0}>
+        <Box mb={4}>
+          <Checkbox
+            mb={1}
+            checked={visible}
+            onChange={() => setVisible((v) => !v)}
+            label="visible"
+          />
 
-        <Input
-          label="Duration"
-          mb={1}
-          onChange={(v) => setDuration(parseInt(v, 10) || 0)}
-          value={duration}
-        />
+          <Input
+            label="Duration"
+            mb={1}
+            onChange={(v) => setDuration(parseInt(v, 10) || 0)}
+            value={duration}
+          />
+        </Box>
       </Box>
-      <Box maxWidth={400}>
-        <args.component duration={duration} visible={visible}>
-          <Card>
-            <Title>I am animated</Title>
-          </Card>
-        </args.component>
+      <Box
+        flex
+        justifyContent="center"
+        alignItems="center"
+        flexGrow={1}
+        flexShrink={0}
+        flexBasis={0}
+        bgColor="gray.50"
+      >
+        <Box maxWidth={400} height={200}>
+          <args.component duration={duration} visible={visible}>
+            <Card>
+              <Title>I am animated</Title>
+            </Card>
+          </args.component>
+        </Box>
       </Box>
     </Box>
   );
