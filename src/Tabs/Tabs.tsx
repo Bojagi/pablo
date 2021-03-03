1ce3d5627e2bf3b26b3f61ff555afa1c6a3d3ae0
Simon Jentsch <simon@bojagi.io>
2021-03-03 01:28:09 +0100

    Fix wrapping/cutting of tabs in dynamic layouts

--- src/Tabs/Tabs.tsx
+++ src/Tabs/Tabs.tsx
@@ -78,6 +78,7 @@ export const Tabs = forwardRef<HTMLDivElement, TabsProps>(
                 `
               : undefined
           }
+          flexWrap="wrap"
           data-testid="pbl-tabs"
           ref={setInnerRef}
           mx={`-${gap}`}
