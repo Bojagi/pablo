20d06616f1c572cd66f6206eadee434d507af821
Simon Jentsch <simon@bojagi.io>
2021-05-18 19:50:25 +0200

    Use components directly

--- src/Show/Show.tsx
+++ src/Show/Show.tsx
@@ -3,13 +3,9 @@ import { mediaQueryOnly } from '../breakpoints/mediaQueryFns';
 import { useBreakpoint } from '../breakpoints/useBreakpoint';
 import { HideAbove, HideBelow, HideProps } from '../Hide';
 
-export function ShowAbove({ breakpoint, children }: HideProps) {
-  return <HideBelow breakpoint={breakpoint}>{children}</HideBelow>;
-}
+export const ShowAbove = HideBelow;
 
-export function ShowBelow({ breakpoint, children }: HideProps) {
-  return <HideAbove breakpoint={breakpoint}>{children}</HideAbove>;
-}
+export const ShowBelow = HideAbove;
 
 export function ShowOnlyOn({ breakpoint, children }: HideProps) {
   const isOnBreakpoint = useBreakpoint(breakpoint, mediaQueryOnly);
