c237ee260e38a9935b60bbec03a981e3affda2bb
Simon Jentsch <simon@bojagi.io>
2021-05-18 20:05:21 +0200

    Show current breakpoint in stories

--- src/Hide/Hide.stories.tsx
+++ src/Hide/Hide.stories.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ShowBelow, ShowOnlyOn } from '../Show';
 import { Tab, Tabs } from '../Tabs';
 import { Breakpoint } from '../theme/breakpoints';
 import { Paragraph } from '../Typography';
@@ -8,13 +9,28 @@ export default {
   title: 'Hide',
 };
 
+const CurrentBreakpoint = () => (
+  <Paragraph>
+    CurrentBreakpoint:
+    <br />
+    <ShowBelow breakpoint="sm">Below Small (mobile)</ShowBelow>
+    <ShowOnlyOn breakpoint="sm">Small (Tablet portrait)</ShowOnlyOn>
+    <ShowOnlyOn breakpoint="md">Medium (Tablet landscape)</ShowOnlyOn>
+    <ShowOnlyOn breakpoint="lg">Large (Desktop landscape)</ShowOnlyOn>
+    <ShowOnlyOn breakpoint="xl">XL Large (Desktop landscape)</ShowOnlyOn>
+  </Paragraph>
+);
+
 const BreakpointSelector = ({ selected, onSelect }) => (
-  <Tabs selected={selected} onSelect={onSelect}>
-    <Tab name="sm">small</Tab>
-    <Tab name="md">medium</Tab>
-    <Tab name="lg">large</Tab>
-    <Tab name="xl">XL</Tab>
-  </Tabs>
+  <>
+    <CurrentBreakpoint />
+    <Tabs selected={selected} onSelect={onSelect}>
+      <Tab name="sm">small</Tab>
+      <Tab name="md">medium</Tab>
+      <Tab name="lg">large</Tab>
+      <Tab name="xl">XL</Tab>
+    </Tabs>
+  </>
 );
 
 export const HideBelowBreakpoint = () => {
--- src/Show/Show.stories.tsx
+++ src/Show/Show.stories.tsx
@@ -8,13 +8,28 @@ export default {
   title: 'Show',
 };
 
+const CurrentBreakpoint = () => (
+  <Paragraph>
+    CurrentBreakpoint:
+    <br />
+    <ShowBelow breakpoint="sm">Below Small (mobile)</ShowBelow>
+    <ShowOnlyOn breakpoint="sm">Small (Tablet portrait)</ShowOnlyOn>
+    <ShowOnlyOn breakpoint="md">Medium (Tablet landscape)</ShowOnlyOn>
+    <ShowOnlyOn breakpoint="lg">Large (Desktop landscape)</ShowOnlyOn>
+    <ShowOnlyOn breakpoint="xl">XL Large (Desktop landscape)</ShowOnlyOn>
+  </Paragraph>
+);
+
 const BreakpointSelector = ({ selected, onSelect }) => (
-  <Tabs selected={selected} onSelect={onSelect}>
-    <Tab name="sm">small</Tab>
-    <Tab name="md">medium</Tab>
-    <Tab name="lg">large</Tab>
-    <Tab name="xl">XL</Tab>
-  </Tabs>
+  <>
+    <CurrentBreakpoint />
+    <Tabs selected={selected} onSelect={onSelect}>
+      <Tab name="sm">small</Tab>
+      <Tab name="md">medium</Tab>
+      <Tab name="lg">large</Tab>
+      <Tab name="xl">XL</Tab>
+    </Tabs>
+  </>
 );
 
 export const ShowBelowBreakpoint = () => {
