10605437c1808febf1c7b425f0108c432099e680
Simon Jentsch <simon@bojagi.io>
2022-04-05 22:25:12 +0200

    fix useBreakpoint media query call

--- src/breakpoints/useBreakpoint.ts
+++ src/breakpoints/useBreakpoint.ts
@@ -1,6 +1,7 @@
 import { useMediaQuery } from '@react-hook/media-query';
 import { usePabloTheme } from '../theme';
 import { Breakpoint } from '../theme/breakpoints';
+import { themeVars } from '../theme/themeVars';
 import { MediaQueryFn, mediaQueryAbove } from './mediaQueryFns';
 
 export function useBreakpoint(
@@ -11,6 +12,8 @@ export function useBreakpoint(
   const breakpointIndex = theme.breakpoints.breakpointNames.findIndex(
     (bp) => bp === breakpointName
   );
-  const mediaQuery = mediaQueryFn(theme.breakpoints, breakpointIndex);
+  const bp = themeVars.breakpoints[breakpointName];
+
+  const mediaQuery = mediaQueryFn(bp, theme.breakpoints, breakpointIndex);
   return useMediaQuery(mediaQuery);
 }
