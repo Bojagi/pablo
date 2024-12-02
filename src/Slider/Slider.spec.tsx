a546f3bd294ae8c8147a7b0e8ad18eba8e351c42
Simon Jentsch <simon@bojagi.io>
2024-12-02 23:41:52 +0100

    Fix lint

--- src/Slider/Slider.spec.tsx
+++ src/Slider/Slider.spec.tsx
@@ -1,7 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { Slider } from './Slider';
-import { composeStories } from '@storybook/react';
 import * as stories from './Slider.stories';
 
 const defaultProps = {
