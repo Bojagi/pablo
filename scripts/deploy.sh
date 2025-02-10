eedbcc766def80b122eb85d84e7280edbc159a80
JohannesMerz <johannes@bojagi.io>
2025-02-10 12:57:28 +0100

    adjust deployment folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,8 @@ echo $VERSION
 
 if [[ $VERSION == *"next"* ]]; then
   echo "pre release - releasing to dist tag next"
-  yarn publish --non-interactive --dist-tag next --access public ./build
+  yarn publish --non-interactive --dist-tag next --access public ./dist
 else
   echo "normal release - releasing to dist tag latest"
-  yarn publish --non-interactive --access public ./build
+  yarn publish --non-interactive --access public ./dist
 fi
